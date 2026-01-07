70781fbe0ba6e42dd5ea7736b804376a868f22f7
enrico8055 <enrico.aurelius@gmail.com>
2026-01-07 19:39:04 +0700

    Initial Commit

---
+++ 01project/_1projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1projectApp.swift
+//  01project
+//
+//  Created by Enrico Aurelius Salim on 07/01/26.
+//
+
+import SwiftUI
+
+@main
+struct _1projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
