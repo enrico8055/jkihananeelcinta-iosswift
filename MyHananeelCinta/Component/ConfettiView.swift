da6b4ac16457a1ec957d2ed6dd335babf548668d
enrico8055 <enrico.aurelius@gmail.com>
2026-01-12 21:16:00 +0700

    support ucapan ulang tahun

---
+++ MyHananeelCinta/Component/ConfettiView.swift
@@ -0,0 +1,18 @@
+//
+//  ConfettiView.swift
+//  MyHananeelCinta
+//
+//  Created by Enrico Aurelius Salim on 12/01/26.
+//
+
+import SwiftUI
+
+struct ConfettiView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConfettiView()
+}
